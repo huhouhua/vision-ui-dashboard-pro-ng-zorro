3b69df486b0caf768f38a7aa4973881a96578d94
huhouhuaM <huhouhuaM@outlook.com>
2023-03-26 22:00:02 +0800

    update project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Firstdemo2
+# vision-ui-dashboard-pro-ng-zorro
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firstdemo2'`, () => {
+  it(`should have as title 'vision-ui-dashboard-pro-ng-zorro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstdemo2');
+    expect(app.title).toEqual('vision-ui-dashboard-pro-ng-zorro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('firstdemo2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('vision-ui-dashboard-pro-ng-zorro app is running!');
   });
 });
